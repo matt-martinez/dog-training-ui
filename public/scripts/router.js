64f3ed98aa011d16d00153c84df0faa2df0e70b1
mattmartinez <mattmartinez816@gmail.com>
2017-03-14 14:34:12 -0700

    Setup app router for homepage

---
+++ public/scripts/router.js
@@ -0,0 +1,19 @@
+angular.module('DogTrainingApp', ['ui.router'])
+  .config(AppRouter)
+  // .config(authInterceptor)
+  //
+  // function authInterceptor($httpProvider) {
+  //   $httpProvider.interceptors.push('AuthInterceptor')
+  // }
+
+  function AppRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('homepage', {
+      url: '/',
+      templateUrl: '/partials/homepage.html'
+    })
+
+  }
