74a534a3a58aa66d4e833d049a0ed6fb582bcea7
mattmartinez <mattmartinez816@gmail.com>
2017-03-15 16:44:33 -0700

    Added createPost function, successfully tested

--- public/scripts/controllers/post_controller.js
+++ public/scripts/controllers/post_controller.js
@@ -17,6 +17,7 @@ function PostController($scope, $http, $state) {
   }
   getAllPosts();
 
+  // Show
   function showPost(id) {
     // console.log("Post Show");
     $http.get(`${server}/posts/${id}`)
@@ -28,6 +29,20 @@ function PostController($scope, $http, $state) {
       });
   }
 
+  // Create
+  self.newPost = {title: '', body: ''};
+  function createPost(currentUser) {
+    // console.log("Post Create")
+    $http.post(`${server}/users/${currentUser.id}/posts`, { post: { title: self.newPost.title, body: self.newPost.body, user_id: currentUser.id }})
+    .then(function(response) {
+      // console.log(response);
+      getAllPosts();
+      self.newPost = {title: '', body: ''};
+      $state.go('forum');
+    });
+  }
+
+  self.createPost = createPost;
   self.showPost = showPost;
   self.getAllPosts = getAllPosts;
 
