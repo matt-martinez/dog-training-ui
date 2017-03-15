e5fa55e681be30925f9f2cfcb501c5a7d3a7be1f
mattmartinez <mattmartinez816@gmail.com>
2017-03-15 14:45:29 -0700

    Added showPost function

--- public/scripts/controllers/post_controller.js
+++ public/scripts/controllers/post_controller.js
@@ -1,22 +1,34 @@
-function PostController($scope, $http) {
+function PostController($scope, $http, $state) {
   var self    = this;
   var server  = 'http://localhost:3000';
   // placeholder for heroku api link
+  // console.log("Loading Post Controller");
 
   self.allPosts = [];
-  getAllPosts();
 
   // Index
   function getAllPosts() {
-    console.log("Loading Posts")
+    // console.log("Loading Posts")
     $http.get(`${server}/posts`)
       .then(function(response) {
-        console.log(response.data.posts);
+        // console.log(response.data.posts);
         self.allPosts = response.data.posts;
       });
   }
+  getAllPosts();
 
+  function showPost(id) {
+    // console.log("Post Show");
+    $http.get(`${server}/posts/${id}`)
+      .then(function(response) {
+        // console.log(response.data.post);
+        self.singlePost = response.data.post;
 
+        $state.go('post');
+      });
+  }
+
+  self.showPost = showPost;
   self.getAllPosts = getAllPosts;
 
 }
