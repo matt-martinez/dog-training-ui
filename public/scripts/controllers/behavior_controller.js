dbf1edcaef37dd744ca93cd0ff1280f842b8980d
mattmartinez <mattmartinez816@gmail.com>
2017-03-16 13:38:47 -0700

    Finished wiring behavior controller

---
+++ public/scripts/controllers/behavior_controller.js
@@ -0,0 +1,8 @@
+function BehaviorController($scope, $http, $state) {
+  var self    = this;
+  var server  = 'http://localhost:3000';
+  // placeholder for heroku api link
+  console.log("Loading Behavior Controller");
+
+
+}
