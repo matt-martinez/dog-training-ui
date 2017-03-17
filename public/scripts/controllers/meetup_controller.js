9b74373e59bb11d9855e3c2aefe43050323c4386
mattmartinez <mattmartinez816@gmail.com>
2017-03-17 13:05:23 -0700

    Added meetup controller and partial

---
+++ public/scripts/controllers/meetup_controller.js
@@ -0,0 +1,7 @@
+function MeetupController($scope, $http, $state) {
+  var self    = this;
+  var server  = 'http://localhost:3000';
+  // placeholder for heroku api link
+  console.log("Loading Meetup Controller");
+
+}
