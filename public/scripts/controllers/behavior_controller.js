af68c60d9c08b36ad0253b1e060468c9b7e6698b
mattmartinez <mattmartinez816@gmail.com>
2017-03-16 23:24:52 -0700

    Cleaned up behavior controller'

--- public/scripts/controllers/behavior_controller.js
+++ public/scripts/controllers/behavior_controller.js
@@ -2,16 +2,17 @@ function BehaviorController($scope, $http, $state) {
   var self    = this;
   var server  = 'http://localhost:3000';
   // placeholder for heroku api link
-  console.log("Loading Behavior Controller");
+  // console.log("Loading Behavior Controller");
 
   self.allBehaviors = [];
 
   function getAllBehaviors() {
-    console.log("Loading Behaviors")
+    // console.log("Loading Behaviors")
     $http.get(`${server}/behaviors`)
       .then(function(response) {
-        console.log(response);
-        console.log(response.data.behaviors);
+        // console.log(response);
+        self.allBehaviors = response.data.behaviors;
+        // console.log(self.allBehaviors);
       });
   }
   getAllBehaviors();
