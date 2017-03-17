b2821f396364d6d3de13abe1e60c86de02cc5d75
mattmartinez <mattmartinez816@gmail.com>
2017-03-16 20:45:21 -0700

    Added showPost get request, editPost, and deletePost from post controller

--- public/scripts/controllers/comment_controller.js
+++ public/scripts/controllers/comment_controller.js
@@ -3,27 +3,62 @@ function CommentController($scope, $http, $state, $stateParams) {
   var server  = 'http://localhost:3000';
   // placeholder for heroku api link
   // console.log("Loading Comment Controller");
-  console.log($stateParams)
+  // console.log($stateParams)
   // console.log($scope.$parent.posts.singlePost.id)
 
   // Show
-  function getPostComments() {
-    // console.log("Loading Comments by Post")
+  function getPostsAndComments() {
+    // console.log("Loading Posts and Comments")
     var id = $stateParams.id;
+    // calling posts
+    $http.get(`${server}/posts/${id}`)
+      .then(function(response) {
+        // console.log(response.data.post);
+        self.singlePost = response.data.post;
+      });
+
+    // calling comments
     $http.get(`${server}/posts/${id}/comments`)
       .then(function(response) {
         // console.log(response);
         self.postComments = response.data.comments;
       });
   }
-  getPostComments();
+  getPostsAndComments();
 
-  // Create
+  // Post Edit
+  function editPost(post, currentUser, id) {
+    // console.log("Post Edit");
+    // console.log(post)
+    // console.log(currentUser)
+    // console.log(id)
+    $http.put(`${server}/posts/${id}`, { post: {title: post.title, body: post.body }})
+      .then(function(response) {
+        console.log(response);
+        getPostsAndComments();
+      });
+  }
 
-  // Edit
+  // Post Delete
+  function deletePost(currentUser, id) {
+    // console.log("Post Delete");
+    // console.log(currentUser)
+    // console.log(id)
+    $http.delete(`${server}/posts/${id}`)
+      .then(function(response) {
+        // console.log(response);
+        $state.go('forum');
+      });
+  }
 
-  // Delete
+  // Comment Create
 
-  self.getPostComments = getPostComments;
+  // Comment Edit
+
+  // Comment Delete
+
+  self.deletePost = deletePost;
+  self.editPost = editPost;
+  self.getPostsAndComments = getPostsAndComments;
 
 }
