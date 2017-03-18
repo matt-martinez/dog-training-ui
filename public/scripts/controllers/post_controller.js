65e6f02e84435e11b28842714603490c1c051530
mattmartinez <mattmartinez816@gmail.com>
2017-03-18 13:59:01 -0700

    Switched server variables

--- public/scripts/controllers/post_controller.js
+++ public/scripts/controllers/post_controller.js
@@ -1,7 +1,7 @@
 function PostController($scope, $http, $state) {
   var self    = this;
-  var server  = 'http://localhost:3000';
-  // var server  = 'https://pawsitive-reinforcement-api.herokuapp.com/';
+  // var server  = 'http://localhost:3000';
+  var server  = 'https://pawsitive-reinforcement-api.herokuapp.com/';
   // console.log("Loading Post Controller");
 
   getAllPosts();
